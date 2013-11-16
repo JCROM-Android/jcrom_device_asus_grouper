7e13d6b0c73a8a1bc1ce29c3e36696de786bb15e
Akihiro Maeda <sola.1980.a@gmail.com>
2013-11-17 01:27:23 +0900

    Changed to use the binary for Android 4.4.

--- full_grouper.mk
+++ full_grouper.mk
@@ -31,12 +31,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 #JCROM
 $(call inherit-product-if-exists, jcrom/asus/grouper/device-grouper.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT=google/nakasi/grouper:4.3/JWR66Y/776638:user/release-keys PRIVATE_BUILD_DESC="nakasi-user 4.3 JWR66Y 776638 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT=google/nakasi/grouper:4.4/KRT16O/907817:user/release-keys PRIVATE_BUILD_DESC="nakasi-user 4.4 KRT16O 907817 release-keys"
 
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_grouper
 PRODUCT_DEVICE := grouper
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 7
-PRODUCT_MANUFACTURER := Asus
+PRODUCT_MANUFACTURER := asus
 
