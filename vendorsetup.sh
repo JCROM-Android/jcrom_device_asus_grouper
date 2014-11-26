09576885701b465d6f3c437d76cb427c457661d5
Akihiro Maeda <sola.1980.a@gmail.com>
2014-11-26 19:17:08 +0900

    Add user to buildtype
    Former-commit-id: aafb3cc5d4f2fc6bc74199ad8c4aad6663a04cac

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,4 +20,7 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
+add_lunch_combo aosp_grouper-user
 add_lunch_combo aosp_grouper-userdebug
+add_lunch_combo full_grouper-user
+add_lunch_combo full_grouper-userdebug
