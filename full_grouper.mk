ce8682c460ab2d2ccec342fe2b0bfff198a8a2ad
Akihiro Maeda <sola.1980.a@gmail.com>
2014-11-26 19:18:19 +0900

    Change PRODUCT_BRAND and PRODUCT_MODEL for products
    Former-commit-id: c5a8b526f58bc87317335ef46d15966294c6ca1c

--- full_grouper.mk
+++ full_grouper.mk
@@ -23,8 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_grouper
 PRODUCT_DEVICE := grouper
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on Grouper
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := asus
 
 #AOSP
 $(call inherit-product-if-exists, vendor/aosp/asus/grouper/full.mk)
